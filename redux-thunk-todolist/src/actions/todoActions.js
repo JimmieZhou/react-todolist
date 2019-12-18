be038a84b097b684142be88e29340bee31025195
JimmieZhou <jimmiezhou@126.com>
2019-12-18 10:40:46 +0800

    🐜 update:完善功能

--- redux-thunk-todolist/src/actions/todoActions.js
+++ redux-thunk-todolist/src/actions/todoActions.js
@@ -6,24 +6,64 @@
  * @LastEditors  : jimmiezhou
  * @LastEditTime : 2019-12-17 22:30:01
  */
-import axios from "axios";
 
 export const INIT_ITEM = "INIT_ITEM";
 export const ADD_ITEM = "ADD_ITEM";
 export const DELETE_ITEM = "DELETE_ITEM";
 export const UPDATE_ITEM = "UPDATE_ITEM";
 
-const base_url = "http://localhost:3001"
+const base_url = "http://localhost:3001";
 
 export const initItem = () => dispatch => {
-  axios.get(`${base_url}/init`).then(function(res) {
-    const item = res.data.data;
-    dispatch({ type: INIT_ITEM, payload: { item } });
-  });
+  fetch(`${base_url}/init`)
+    .then(data => data.json())
+    .then(result => {
+      const item = result.data;
+      dispatch({ type: INIT_ITEM, payload: { item } });
+    });
 };
 
-export const addItem = item => ({ type: ADD_ITEM, payload: { item } });
+export const addItem = item => dispatch => {
+  fetch(`${base_url}/add`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(item)
+  })
+    .then(data => data.json())
+    .then(result => {
+      const item = result.data;
+      dispatch({ type: ADD_ITEM, payload: { item } });
+    });
+};
 
-export const deleteItem = item => ({ type: DELETE_ITEM, payload: { item } });
+export const deleteItem = item => dispatch => {
+  fetch(`${base_url}/delete`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(item)
+  })
+    .then(data => data.json())
+    .then(result => {
+      const item = result.data;
+      dispatch({ type: DELETE_ITEM, payload: { item } });
+    });
+};
 
-export const updateItem = item => ({ type: UPDATE_ITEM, payload: { item } });
+export const updateItem = item => dispatch => {
+  fetch(`${base_url}/update`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(item)
+  })
+    .then(data => data.json())
+    .then(result => {
+      const item = result.data;
+      dispatch({ type: UPDATE_ITEM, payload: { item } });
+    });
+};
