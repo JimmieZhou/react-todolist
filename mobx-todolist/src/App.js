38a220de321bd9aeaf9984e3b1731b6ec4c292fe
JimmieZhou <jimmiezhou@126.com>
2019-12-18 15:00:47 +0800

    🐝 add:mobx-todolis

--- mobx-todolist/src/App.js
+++ mobx-todolist/src/App.js
@@ -3,13 +3,12 @@
  * @version: 1.0.0
  * @Author: jimmiezhou
  * @Date: 1985-10-26 16:15:00
- * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-12-16 18:06:44
+ * @LastEditors  : jimmiezhou
+ * @LastEditTime : 2019-12-18 14:59:49
  */
 import React from "react";
 import "./App.css";
 import TodoListContaner from "./containers/TodoListContaner";
-
 class App extends React.Component {
   render() {
     return <TodoListContaner></TodoListContaner>;
